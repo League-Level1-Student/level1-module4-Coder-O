5dccc7d7282f47be77a130eeb07dda51c7f9d44d
Owen <owemarco@gmail.com>
2020-10-26 13:25:46 -0700

    On 1-4-6: Book of Illusions

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,15 +1,26 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TypingTutor implements KeyListener {
 	char currentLetter;
+	JLabel label;
+	JFrame frame;
+	JPanel panel;
+	Color green;
+	Color red;
+	int charsTyped;
+	
+	Date timeAtStart;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TypingTutor tutor = new TypingTutor();
@@ -22,10 +33,10 @@ public class TypingTutor implements KeyListener {
 	}
 	
 	void setup() {
-		JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+		frame = new JFrame();
+        panel = new JPanel();
         currentLetter = generateRandomLetter();
-        JLabel label = new JLabel();
+        label = new JLabel();
         label.setText(String.valueOf(currentLetter));
         label.setFont(label.getFont().deriveFont(28.0f));
         label.setHorizontalAlignment(JLabel.CENTER);
@@ -35,24 +46,56 @@ public class TypingTutor implements KeyListener {
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
+        
+        green = new Color(0,255,0);
+        red = new Color(255, 0, 0);
+        
+        timeAtStart = new Date();
+        charsTyped = 0;
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("you typed: " + e.getKeyChar());
+		if(e.getKeyChar() == currentLetter) {
+			System.out.println("Correct!");
+			panel.setBackground(green);
+		}
+		else {
+			panel.setBackground(red);
+		}
+		charsTyped++;
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText(String.valueOf(currentLetter));
+		if(charsTyped>=15) {
+			showTypingSpeed(charsTyped);
+			charsTyped = 0;
+			timeAtStart = new Date();
+		}
 		
+		
+	}
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+	    Date timeAtEnd = new Date();
+	    long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+	    long gameInSeconds = (gameDuration / 1000) % 60;
+	    double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+	    int charactersPerMinute = (int) (charactersPerSecond * 60);
+	    JOptionPane.showMessageDialog(null, "Your typing speed is " + charactersPerMinute + " characters per minute.");
 	}
 
 }
